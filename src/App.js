1992d2164740f596c86f693eefb48d73a3e33131
SunWukong97 <34043095+ThomasNLy@users.noreply.github.com>
2023-01-31 20:46:23 -0500

    utilize custom to do list element
    replaced old code for rendering to do list with new to do list element for organization and to easily design how it shall look

--- src/App.js
+++ src/App.js
@@ -1,28 +1,32 @@
-import logo from "./logo.svg";
 import React, { useState } from "react";
 import "./App.css";
 import InputField from "./InputField";
+import ToDoItem from "./ToDoItem";
 function App() {
   const [tasks, addTasks] = useState([]);
 
-  let deleteItem = (item) => {
-  
+  let deleteItem = (itemID) => {
     let newList = [];
-    tasks.forEach((task) => {
-      
-      if (task !== item) {
+    tasks.forEach((task, index) => {
+      let currentID = `${index}${task}`;
+      if (currentID !== itemID) {
         console.log("found it");
         newList.push(task);
       }
     });
     addTasks(newList);
   };
-  let listOfTasks = tasks.map((t) => {
+  let listOfTasks = tasks.map((task, index) => {
+    let id = `${index}${task}`;
     return (
-      <div key={t}>
-        <p>{t}</p>
-        <button onClick={() => deleteItem(t)}>delete</button>
-      </div>
+      <ToDoItem key={id} id={id} task={task} deleteItem={deleteItem} />
+      // <div key={id}>
+      //   <p>{task}</p>
+      //   {/* on click needs to have a function that tells it what to do
+      //   if clicking the button requires parameters to be passed than it needs to be a function called within the onclick function
+      //   */}
+      //   <button onClick={() => deleteItem(id)}>delete</button>
+      // </div>
     );
   });
   console.log(tasks);
