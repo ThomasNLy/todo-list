9de8477f18cf3d5cbf6528c168586b8f9d8fa89f
SunWukong97 <34043095+ThomasNLy@users.noreply.github.com>
2023-02-03 10:24:28 -0500

    added a delete button to remove a completed task
    added a delete button to remove a completed task along with styling a custom check mark button

--- src/ToDoItem.jsx
+++ src/ToDoItem.jsx
@@ -1,15 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ToDoItem.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faX } from "@fortawesome/free-solid-svg-icons";
 function ToDoItem(props) {
   const { id, task, deleteItem } = props;
+  const [check, setCheck] = useState(false);
+
+  let isChecked = () => {
+    setCheck(!check);
+  };
 
   return (
-    <div className="toDoItem">
-      <p>{task}</p>
+    <div className="to-do-item-container">
+      <div className="checkmark-container">
+        <input
+          className="checkbox-round"
+          type="checkbox"
+          name="toDoItem"
+          value={task}
+          onChange={isChecked}
+        />
+      </div>
+
+      <div className="task-container">
+        <label htmlFor="toDoItem" className="checkbox-label">
+          {task}
+        </label>
+        <p>date</p>
+      </div>
+
+      {/* <p>{task}</p> */}
       {/* on click needs to have a function that tells it what to do
         if clicking the button requires parameters to be passed than it needs to be a function called within the onclick function
         */}
-      <button onClick={() => deleteItem(id)}>delete</button>
+      <button className="delete-button" onClick={() => deleteItem(id)}>
+        {" "}
+        <FontAwesomeIcon icon={faX} inverse />
+      </button>
     </div>
   );
 }
